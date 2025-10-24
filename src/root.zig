4d45da945330e4ea6607c6903dfe73daa96375be
Tom Ford <tfordy63@gmail.com>
2025-10-24 12:28:22 +0200

    links

--- src/root.zig
+++ src/root.zig
@@ -2,3 +2,9 @@ const std = @import("std");
 const sqlite = @cImport({
     @cInclude("sqlite3.h");
 });
+
+test "sqlite linking" {
+    const version = sqlite.sqlite3_libversion();
+    std.debug.print("SQLite version: {s}\n", .{version});
+    try std.testing.expect(version != null);
+}
